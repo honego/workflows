45791c5ef99479f00f19ac56ad49c19aa84f94eb
havario <157877551+havario@users.noreply.github.com>
2026-01-13 14:57:12 +0800

    update nxtrace.sh
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- nxtrace.sh
+++ nxtrace.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0
 #
-# Modified from the project: https://github.com/nxtrace/NTrace-V1
+# Based on from: https://github.com/nxtrace/NTrace-core
+#                https://github.com/nxtrace/NTrace-V1
 # Description: This script installs or updates the latest nexttrace version, overcoming the official script's restriction to only stable versions.
 # Copyright (c) 2025-2026 honeok <i@honeok.com>
 
@@ -14,36 +15,78 @@ _err_msg() { printf "\033[41m\033[1mError\033[0m %b\n" "$*"; }
 _suc_msg() { printf "\033[42m\033[1mSuccess\033[0m %b\n" "$*"; }
 _info_msg() { printf "\033[43m\033[1mInfo\033[0m %b\n" "$*"; }
 
-# 各变量默认值
+# Default variable values
 TEMP_DIR="$(mktemp -d)"
 GITHUB_PROXY='https://v6.gh-proxy.org/'
 
 trap 'rm -rf "${TEMP_DIR:?}" > /dev/null 2>&1' INT TERM EXIT
 
+# Strip "v" prefix if present
+VERSION="${VERSION#v}"
+
+# The channel to install from:
+#   * stable
+#   * dev
+DEFAULT_CHANNEL_VALUE="stable"
+if [ -z "$CHANNEL" ]; then
+    CHANNEL="$DEFAULT_CHANNEL_VALUE"
+fi
+
 clear() {
     [ -t 1 ] && tput clear 2> /dev/null || printf "\033[2J\033[H" || command clear
 }
 
-# 打印错误信息并退出
+# Print error message and exit
 die() {
     _err_msg >&2 "$(_red "$@")"
     exit 1
 }
 
-# 临时工作目录
-cd "$TEMP_DIR" > /dev/null 2>&1 || die "无法进入工作路径"
+cd "$TEMP_DIR" > /dev/null 2>&1 || die "Can't access temporary work dir."
 
-_exists() {
-    local _CMD="$1"
-    if type "$_CMD" > /dev/null 2>&1; then
-        return
-    elif command -v "$_CMD" > /dev/null 2>&1; then
-        return
-    elif which "$_CMD" > /dev/null 2>&1; then
-        return
-    else
-        return 1
-    fi
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+    --channel)
+        CHANNEL="$2"
+        shift
+        ;;
+    --version)
+        VERSION="${2#v}"
+        shift
+        ;;
+    --debug)
+        set -x
+        shift
+        ;;
+    --*)
+        _yellow "Illegal option $1"
+        ;;
+    esac
+    shift $(($# > 0 ? 1 : 0))
+done
+
+case "$CHANNEL" in
+stable)
+    DOWNLOAD_URL="https://github.com/nxtrace/NTrace-core"
+    DOWNLOAD_API_URL="https://api.github.com/repos/nxtrace/NTrace-core/releases"
+    ;;
+dev)
+    DOWNLOAD_URL="https://github.com/nxtrace/NTrace-V1"
+    DOWNLOAD_API_URL="https://api.github.com/repos/nxtrace/NTrace-V1/releases"
+    ;;
+*)
+    die "unknown CHANNEL $CHANNEL: use either stable or dev."
+    ;;
+esac
+
+get_cmd_path() {
+    # -f: 忽略shell内置命令和函数, 只考虑外部命令
+    # -p: 只输出外部命令的完整路径
+    type -f -p "$1"
+}
+
+is_have_cmd() {
+    get_cmd_path "$1" > /dev/null 2>&1
 }
 
 curl() {
@@ -66,10 +109,8 @@ curl() {
     done
 }
 
-check_root() {
-    if [ "$EUID" -ne 0 ] || [ "$(id -ru)" -ne 0 ]; then
-        die "此脚本必须以root身份运行"
-    fi
+is_not_root() {
+    [ "$(id -u)" -ne 0 ]
 }
 
 check_cdn() {
@@ -78,70 +119,94 @@ check_cdn() {
     fi
 }
 
+is_darwin() {
+    [ "$(uname -s 2> /dev/null)" = "Darwin" ]
+}
+
+is_linux() {
+    [ "$(uname -s 2> /dev/null)" = "Linux" ]
+}
+
+is_writable() {
+    [ -w "$1" ]
+}
+
+is_executable() {
+    [ -x "$1" ]
+}
+
 check_sys() {
-    case "$(uname -s 2> /dev/null | sed 's/.*/\L&/')" in
-    linux) OS_NAME="linux" ;;
-    *) die "系统不被支持" ;;
-    esac
+    if is_linux; then
+        OS_NAME="linux"
+    elif is_darwin; then
+        OS_NAME="darwin"
+    else
+        die "System is not supported."
+    fi
 }
 
 check_arch() {
-    case "$(uname -m 2> /dev/null)" in
-    i*86) OS_ARCH="386" ;;
-    amd64 | x86_64) OS_ARCH="amd64" ;;
-    arm64 | armv8 | aarch64) OS_ARCH="arm64" ;;
-    armv7*) OS_ARCH="armv7" ;;
-    mips) OS_ARCH="mips" ;;
-    *) die "架构不被支持" ;;
-    esac
+    if is_linux; then
+        case "$(uname -m 2> /dev/null)" in
+        i*86) OS_ARCH="386" ;;
+        amd64 | x86_64) OS_ARCH="amd64" ;;
+        arm64 | armv8 | aarch64) OS_ARCH="arm64" ;;
+        armv7*) OS_ARCH="armv7" ;;
+        mips) OS_ARCH="mips" ;;
+        *) die "Architecture is not supported." ;;
+        esac
+    elif is_darwin; then
+        case "$(uname -m 2> /dev/null)" in
+        amd64 | x86_64) OS_ARCH="amd64" ;;
+        arm64 | armv8 | aarch64) OS_ARCH="arm64" ;;
+        *) die "Architecture is not supported." ;;
+        esac
+    else
+        die "Architecture is not supported."
+    fi
 }
 
-work_dir() {
-    if [ -w "/usr/local/bin" ]; then
+do_install() {
+
+    if is_have_cmd nexttrace; then
+        tee >&2 <<- 'EOF'
+			Warning: the "nexttrace" command appears to already exist on this system.
+            Press Ctrl +C to abort this script if you do not want to overwrite it.
+		EOF
+        (sleep 20)
+    fi
+
+    if is_not_root; then
+        die "This installer needs the ability to run commands as root."
+    fi
+
+    check_sys
+    check_arch
+
+    [ -n "$VERSION" ] || VERSION="$(curl -Ls "${GITHUB_PROXY}${DOWNLOAD_API_URL}" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sort -rV | head -n 1)"
+    curl -L "${GITHUB_PROXY}${DOWNLOAD_URL}/releases/download/${VERSION}/nexttrace_${OS_NAME}_${OS_ARCH}" -o nexttrace ||
+        die "NextTrace download failed."
+
+    if is_writable "/usr/local/bin"; then
         BIN_WORKDIR="/usr/local/bin/nexttrace"
     else
         BIN_WORKDIR="/usr/bin/nexttrace"
     fi
-}
 
-ntrace_down() {
-    local NTRACE_VER
-
-    _info_msg "$(_yellow "获取最新NextTrace发行版文件信息")"
-
-    NTRACE_VER="$(curl -Ls "${GITHUB_PROXY}https://api.github.com/repos/nxtrace/NTrace-V1/releases" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sort -Vr | head -n1)"
-    [ -n "$NTRACE_VER" ] || die "获取版本信息失败, 请检查您的网络是否正常"
-
-    if ! curl -Ls "${GITHUB_PROXY}https://github.com/nxtrace/NTrace-V1/releases/download/$NTRACE_VER/nexttrace_${OS_NAME}_${OS_ARCH}" -o nexttrace; then
-        die "NextTrace下载失败, 请检查您的网络是否正常"
-    fi
-
-    if [ ! -x ./nexttrace ]; then
+    if is_executable ./nexttrace; then
         chmod +x ./nexttrace
     fi
 
     mv -f ./nexttrace "$BIN_WORKDIR"
 
-    if _exists nexttrace; then
-        _suc_msg "$(_green "NextTrace现在已经在您的系统中可用")"
-    else
-        die "NextTrace安装失败, 请重试"
-    fi
-}
-
-ntrace_info() {
-    if [ -e "$BIN_WORKDIR" ]; then
+    if is_have_cmd nexttrace; then
+        _suc_msg "$(_green "NextTrace is now available on your system.")"
         "$BIN_WORKDIR" --version
-        _suc_msg "一切准备就绪! 使用命令 nexttrace 1.1.1.1 开始您的第一次路由测试吧, 更多进阶命令玩法可以用 nexttrace -h 查看"
-        echo "关于软件卸载, 因为nexttrace是绿色版单文件, 卸载只需输入命令 rm $BIN_WORKDIR 即可"
+    else
+        die "NextTrace installation failed, please try again"
     fi
 }
 
 clear
-check_root
 check_cdn
-check_sys
-check_arch
-work_dir
-ntrace_down
-ntrace_info
+do_install

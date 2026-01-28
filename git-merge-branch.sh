a10d3b9613ca2ed1b34f18533b43b20c012340cf
havario <157877551+havario@users.noreply.github.com>
2026-01-29 04:17:29 +0800

    fix log
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- git-merge-branch.sh
+++ git-merge-branch.sh
@@ -4,7 +4,7 @@
 
 # git merge "$MERGE_BRANCH" --no-ff --no-edit --log --signoff
 
-set -eEu
+set -eEuo pipefail
 
 MERGE_BRANCH="${1:?Usage: $0 <branch-name>}"
 DEFAULT_BRANCH="$(git symbolic-ref refs/remotes/origin/HEAD --short | sed 's|.*/||' 2> /dev/null ||
@@ -16,10 +16,19 @@ git pull --rebase origin "$CURRENT_BRANCH" > /dev/null 2>&1
 # Master: ... -- M1 -- M2 -- M3 (HEAD)
 #                 \
 # Branch:          R1 -- R2 -- R3
-CHANGELOG="$(git log --pretty=format:'* %h %s' "HEAD..$MERGE_BRANCH")"
+RAW_LOG="$(git log --pretty=format:'%h %s' "HEAD..$MERGE_BRANCH")"
+
+# 格式化日志
+INDENTED_LOG="  ${RAW_LOG//$'\n'/$'\n'  }"
+
+# 拼接日志
+# * branch name:
+#   hash message
+CHANGELOG="* $MERGE_BRANCH:
+$INDENTED_LOG"
 
 # 执行合并但不提交
-# --no-ff: 强制生成Merge Commit 保留分支历史
+# --no-ff: 强制生成Merge Commit保留分支历史
 # --no-commit: 暂停提交 允许修改Commit Message
 git merge "$MERGE_BRANCH" --no-ff --no-commit
 
@@ -27,7 +36,9 @@ git merge "$MERGE_BRANCH" --no-ff --no-commit
 if [ "$CURRENT_BRANCH" = "$DEFAULT_BRANCH" ]; then
     git commit -m "Merge branch '$MERGE_BRANCH'" -m "$CHANGELOG" --signoff
 else
-    git commit -m "Merge branch $MERGE_BRANCH into $CURRENT_BRANCH" -m "$CHANGELOG" --signoff
+    git commit -m "Merge branch '$MERGE_BRANCH' into $CURRENT_BRANCH" -m "$CHANGELOG" --signoff
 fi
 
 git push origin "$CURRENT_BRANCH"
+
+echo "Success"

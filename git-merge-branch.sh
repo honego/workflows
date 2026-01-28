c092211db963cb0d5b4c57610767d76793e1f27e
havario <157877551+havario@users.noreply.github.com>
2026-01-29 03:43:48 +0800

    fix
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- git-merge-branch.sh
+++ git-merge-branch.sh
@@ -2,22 +2,32 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2026 honeok <i@honeok.com>
 
-# git merge "$BRANCH" --no-ff --no-edit --log --signoff
+# git merge "$MERGE_BRANCH" --no-ff --no-edit --log --signoff
 
 set -eEu
 
-BRANCH="${1:?Usage: $0 <branch-name>}"
+MERGE_BRANCH="${1:?Usage: $0 <branch-name>}"
+DEFAULT_BRANCH="$(git symbolic-ref refs/remotes/origin/HEAD --short | sed 's|.*/||' 2> /dev/null ||
+    git ls-remote --symref origin HEAD | sed -n 's|^ref: refs/heads/\([^[:space:]]*\).*|\1|p' 2> /dev/null)"
+CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
+
+git pull --rebase origin "$CURRENT_BRANCH" > /dev/null 2>&1
 
 # Master: ... -- M1 -- M2 -- M3 (HEAD)
 #                 \
 # Branch:          R1 -- R2 -- R3
-CHANGELOG="$(git log --pretty=format:'* %h %s' "HEAD..$BRANCH")"
+CHANGELOG="$(git log --pretty=format:'* %h %s' "HEAD..$MERGE_BRANCH")"
 
 # 执行合并但不提交
 # --no-ff: 强制生成Merge Commit 保留分支历史
 # --no-commit: 暂停提交 允许修改Commit Message
-git merge "$BRANCH" --no-ff --no-commit
+git merge "$MERGE_BRANCH" --no-ff --no-commit
 
 # 提交并写入changelog
-git commit -m "Merge branch '$BRANCH'" -m "$CHANGELOG" --signoff
-git push origin "$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
+if [ "$CURRENT_BRANCH" = "$DEFAULT_BRANCH" ]; then
+    git commit -m "Merge branch '$MERGE_BRANCH'" -m "$CHANGELOG" --signoff
+else
+    git commit -m "Merge branch $MERGE_BRANCH into $CURRENT_BRANCH" -m "$CHANGELOG" --signoff
+fi
+
+git push origin "$CURRENT_BRANCH"

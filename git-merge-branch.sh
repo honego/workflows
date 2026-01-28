5b84bea6ecec80426201ce9f718d79c0a99d8b19
havario <157877551+havario@users.noreply.github.com>
2026-01-29 01:05:15 +0800

    Сука блядь
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

---
+++ git-merge-branch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2026 honeok <i@honeok.com>
+
+# git merge "$BRANCH" --no-ff --no-edit --log --signoff
+
+set -eEu
+
+BRANCH="$1"
+
+# Master: ... -- M1 -- M2 -- M3 (HEAD)
+#                 \
+# Branch:          R1 -- R2 -- R3
+CHANGELOG="$(git log --pretty=format:'* %h %s' "HEAD..$BRANCH")"
+
+# 执行合并但不提交
+# --no-ff: 强制生成Merge Commit 保留分支历史
+# --no-commit: 暂停提交 允许修改Commit Message
+git merge "$BRANCH" --no-ff --no-commit
+
+# 提交并写入changelog
+git commit -m "Merge branch '$BRANCH'" -m "$CHANGELOG" --signoff

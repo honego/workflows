f6590e332557930a6a2ece0ed3df1565e3c519f1
havario <157877551+havario@users.noreply.github.com>
2026-02-15 23:50:40 +0800

    1
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- config/shadowsocks-rust/install.sh
+++ config/shadowsocks-rust/install.sh
@@ -4,10 +4,20 @@
 
 set -eE
 
+# 各变量默认值
+TEMP_DIR="$(mktemp -d)"
+CORE_NAME="shadowsocks-rust"
+CORE_DIR="/etc/$CORE_NAME"
+
+# 终止信号捕获
+trap 'rm -rf "${TEMP_DIR:?}" > /dev/null 2>&1' INT TERM EXIT
+
 die() {
     echo >&2 "Error: $*"
     exit 1
 }
+
+cd "$TEMP_DIR" > /dev/null 2>&1 || die "Unable to enter the work path."
 
 is_alpine() {
     [ -f /etc/alpine-release ]
@@ -21,8 +31,11 @@ check_glibc() {
     fi
 }
 
-ss_install() {
-    SS_VERSION="$(curl -Ls https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases | grep -m1 '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+install_ss() {
+    local SS_VERSION OS_ARCH
+    local -a FILENAMES
+
+    SS_VERSION="$(curl -Ls https://api.github.com/repos/shadowsocks/$CORE_NAME/releases | grep -m1 '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
 
     case "$(uname -m 2> /dev/null)" in
     amd64 | x86_64) OS_ARCH="x86_64" ;;
@@ -32,12 +45,35 @@ ss_install() {
 
     FILENAMES=("shadowsocks-$SS_VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz" "shadowsocks-$SS_VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz.sha256")
     for f in "${FILENAMES[@]}"; do
-        curl -Ls -O "https://github.com/shadowsocks/shadowsocks-rust/releases/download/$SS_VERSION/$f"
+        curl -Ls -O "https://github.com/shadowsocks/$CORE_NAME/releases/download/$SS_VERSION/$f"
     done
     sha256sum -c "shadowsocks-$SS_VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz.sha256" > /dev/null 2>&1 || die "checksum verification failed."
     tar fJx "shadowsocks-$SS_VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz"
-    mv -f ssserver /usr/local/bin/ssserver
+    chmod +x ss*
+    mv -f ssserver /usr/local/bin
+}
+
+install_service() {
+    tee > /etc/systemd/system/shadowsocks.service <<- EOF
+[Unit]
+Description=Shadowsocks Rust Server
+Documentation=
+After=network.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/local/bin/ssserver -c $CORE_DIR/config.json
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    systemctl daemon-reload
+    systemctl enable --now shadowsocks.service
 }
 
 check_glibc
-ss_install
+
+install_ss
+install_service

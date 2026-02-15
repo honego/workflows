0b708190469c630f460b582bc5c4d98cc3927ff7
havario <157877551+havario@users.noreply.github.com>
2026-02-16 06:26:58 +0800

    1
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- config/shadowsocks-rust/install.sh
+++ config/shadowsocks-rust/install.sh
@@ -11,7 +11,8 @@ set -eE
 
 # 各变量默认值
 TEMP_DIR="$(mktemp -d)"
-CORE_NAME="shadowsocks-rust"
+PROJECT_NAME="shadowsocks"
+CORE_NAME="$PROJECT_NAME-rust"
 CORE_DIR="/etc/$CORE_NAME"
 
 # 终止信号捕获
@@ -80,19 +81,11 @@ get_ip() {
     echo "$PUBLIC_IP"
 }
 
-check_glibc() {
-    if is_alpine; then
-        GLIBC="musl"
-    else
-        GLIBC="gnu"
-    fi
-}
-
 install_ss() {
-    local SS_VERSION OS_ARCH
+    local OS_ARCH GLIBC
     local -a FILENAMES
 
-    SS_VERSION="$(curl -Ls https://api.github.com/repos/shadowsocks/$CORE_NAME/releases | grep -m1 '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+    [ -n "$VERSION" ] || VERSION="$(curl -Ls https://api.github.com/repos/$PROJECT_NAME/$CORE_NAME/releases | grep -m1 '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
 
     case "$(uname -m 2> /dev/null)" in
     amd64 | x86_64) OS_ARCH="x86_64" ;;
@@ -100,12 +93,18 @@ install_ss() {
     *) die "unsupported cpu architecture." ;;
     esac
 
-    FILENAMES=("shadowsocks-$SS_VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz" "shadowsocks-$SS_VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz.sha256")
+    if is_alpine; then
+        GLIBC="musl"
+    else
+        GLIBC="gnu"
+    fi
+
+    FILENAMES=("$PROJECT_NAME-$VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz" "$PROJECT_NAME-$VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz.sha256")
     for f in "${FILENAMES[@]}"; do
-        curl -Ls -O "https://github.com/shadowsocks/$CORE_NAME/releases/download/$SS_VERSION/$f"
+        curl -Ls -O "https://github.com/$PROJECT_NAME/$CORE_NAME/releases/download/$VERSION/$f"
     done
-    sha256sum -c "shadowsocks-$SS_VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz.sha256" > /dev/null 2>&1 || die "checksum verification failed."
-    tar fJx "shadowsocks-$SS_VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz"
+    sha256sum -c "$PROJECT_NAME-$VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz.sha256" > /dev/null 2>&1 || die "checksum verification failed."
+    tar fJx "$PROJECT_NAME-$VERSION.$OS_ARCH-unknown-linux-$GLIBC.tar.xz"
     chmod +x ss*
     mv -f ss* /usr/local/bin
 }
@@ -134,10 +133,10 @@ EOF
 }
 
 install_service() {
-    tee > /etc/systemd/system/shadowsocks.service <<- EOF
+    tee > /etc/systemd/system/$PROJECT_NAME.service <<- EOF
 [Unit]
 Description=Shadowsocks-rust Server Service
-Documentation=https://github.com/shadowsocks/shadowsocks-rust
+Documentation=https://github.com/$PROJECT_NAME/$CORE_NAME
 After=network.target
 
 [Service]
@@ -152,10 +151,8 @@ Restart=on-failure
 WantedBy=multi-user.target
 EOF
     systemctl daemon-reload
-    systemctl enable --now shadowsocks.service
+    systemctl enable --now "$PROJECT_NAME.service"
 }
-
-check_glibc
 
 install_ss
 gen_cfg

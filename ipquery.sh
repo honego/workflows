f8215dd360a6121df391d48af75e54194134fe3c
havario <157877551+havario@users.noreply.github.com>
2026-02-25 10:46:01 +0800

    1
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- ipquery.sh
+++ ipquery.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+# https://www.nodeseek.com/post-627595-1
 curl -L -4 https://abiding-cistern-488411-j3.uc.r.appspot.com
 curl -L -6 https://abiding-cistern-488411-j3.uc.r.appspot.com

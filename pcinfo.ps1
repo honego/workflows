0b9eada6825723d14c6499ba8cf2e2a64e351f89
havario <157877551+havario@users.noreply.github.com>
2026-03-14 16:02:55 +0800

    1
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- pcinfo.ps1
+++ pcinfo.ps1
@@ -1,7 +1,7 @@
 ﻿# SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2026 honeok <i@honeok.com>
 
-# 系统信息提取
+# 系统信息
 $os = Get-CimInstance Win32_OperatingSystem
 Write-Host "系统版本   : $($os.Caption) ($($os.OSArchitecture))" -ForegroundColor Cyan
 
@@ -10,18 +10,19 @@ $c = Get-CimInstance Win32_Processor
 Write-Host "CPU 型号   : $($c.Name)" -ForegroundColor Cyan
 Write-Host "CPU 数量   : $($c.NumberOfCores) 核心 / $($c.NumberOfLogicalProcessors) 线程" -ForegroundColor Cyan
 
-# 内存信息
+# 内存信息 (总量 + 插槽明细)
 $m = Get-CimInstance Win32_PhysicalMemory
 $totalMem = 0; foreach($st in $m){ $totalMem += $st.Capacity }
 Write-Host "内存       : $([math]::Round($totalMem / 1GB, 2)) GB" -ForegroundColor Cyan
+foreach ($stick in $m) {
+    Write-Host "  └─ 插槽  : $($stick.DeviceLocator) | $($stick.Manufacturer) | $([math]::Round($stick.Capacity / 1GB, 0)) GB | $($stick.Speed) MHz" -ForegroundColor Cyan
+}
 
-# 获取主板核心信息
+# 主板信息
 $b = Get-CimInstance Win32_BaseBoard
 Write-Host "主板型号   : $($b.Manufacturer) $($b.Product)" -ForegroundColor Cyan
 
-# GPU
-# AdapterRAM > 1GB 物理独显基准线 过滤虚拟显卡和共享显存的核显
-# 排除名称包含 Intel 或 Virtual 的设备 防止 Intel 旗舰核显干扰
+# GPU 信息
 $g = Get-CimInstance Win32_VideoController | Where-Object {
     $_.AdapterRAM -ge 1GB -and
     $_.Name -notmatch "Intel|Virtual|Microsoft|Basic|UHD|Iris"

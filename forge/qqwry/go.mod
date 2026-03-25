673d61085e74c254adf1b9b6b3de651390f5f347
havario <157877551+havario@users.noreply.github.com>
2026-03-25 11:31:51 +0800

    delete
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- forge/qqwry/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/honeok/tools/forge/qqwry
-
-go 1.25.6
-
-require github.com/ipipdotnet/ipdb-go v1.3.3
--- sha256sum.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find . -type f -exec dos2unix {} +
-
-# find . -type f -exec touch -mt "$(date +%Y01010000)" {} +
-
-find . -type f ! -name 'checksum.txt' -print0 |
-    sort -z |
-    xargs -0 sha256sum > checksum.txt

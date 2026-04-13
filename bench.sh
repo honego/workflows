de2a9e67adc22631ecf5beaba245f04a7ac9d296
havario <157877551+havario@users.noreply.github.com>
2026-04-13 19:03:19 +0800

    1
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- bench.sh
+++ bench.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 ## 基本系统信息
-# CPU信息
-CPU_MODEL="$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')"
-echo -e "Processor\t: $CPU_MODEL"
+get_system_info() {
+    # CPU信息
+    CPU_MODEL="$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')"
+    CPU_CORES="$(awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo 2> /dev/null)"
+    CPU_FREQ="$(awk -F: ' /cpu MHz/ {freq=$2} END {print freq " MHz"}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')"
 
-CPU_CORES="$(awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo 2> /dev/null)"
-CPU_FREQ="$(awk -F: ' /cpu MHz/ {freq=$2} END {print freq " MHz"}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')"
-echo -e "CPU cores\t: $CPU_CORES @ $CPU_FREQ"
+}
+
+print_system_info() {
+    echo -e "CPU Model\t: $CPU_MODEL"
+    echo -e "CPU Cores\t: $CPU_CORES"
+    echo -e "CPU Frequency\t: $CPU_FREQ"
+}
+
+get_system_info
+print_system_info

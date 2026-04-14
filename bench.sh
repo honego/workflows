08c4ca94cef365a843145dc8b0805a4893cec175
havario <157877551+havario@users.noreply.github.com>
2026-04-14 23:28:53 +0800

    1
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- bench.sh
+++ bench.sh
@@ -63,7 +63,7 @@ format_bytes() {
 }
 
 ## 基本系统信息
-# 获取CPU信息
+# 系统信息模块 -> 获取CPU信息
 get_cpu_info() {
     local cpu_model cpu_cores cpu_freq
     local cpu_l1_cache cpu_l2_cache cpu_l3_cache cache_level cache_type cache_size cache_bytes
@@ -125,7 +125,7 @@ get_cpu_info() {
     RESULT_CPU_VIRT="$cpu_virt"
 }
 
-# 获取内存信息
+# 系统信息模块 -> 获取内存及Swap信息
 get_mem_info() {
     local mem_total mem_available mem_free mem_buffers mem_cached mem_used mem_total_bytes mem_used_bytes
     local swap_total swap_free swap_used swap_total_bytes swap_used_bytes
@@ -168,10 +168,57 @@ get_mem_info() {
     fi
 }
 
+# 系统信息模块 -> 获取磁盘信息
+get_disk_info() {
+    local disk_root_path disk_total disk_used disk_free disk_total_bytes disk_used_bytes disk_free_bytes
+    local fs_path fs_total_kib fs_used_kib fs_free_kib seen_devices
+
+    # 根分区实际对应的设备路径
+    if is_have_cmd findmnt; then
+        disk_root_path="$(findmnt -n -o SOURCE / 2> /dev/null)"
+    else
+        disk_root_path="$(df -kP / 2> /dev/null | awk 'NR==2 {print $1}')"
+    fi
+
+    # 整机磁盘空间汇总
+    disk_total=0
+    disk_used=0
+    disk_free=0
+    seen_devices=$'\n'
+    {
+        read -r _
+        while read -r fs_path fs_total_kib fs_used_kib fs_free_kib _; do
+            [[ "$fs_path" == /dev/* ]] || continue
+
+            case "$seen_devices" in
+            *$'\n'"$fs_path"$'\n'*) continue ;;
+            esac
+
+            seen_devices+="$fs_path"$'\n'
+
+            ((disk_total += fs_total_kib))
+            ((disk_used += fs_used_kib))
+            ((disk_free += fs_free_kib))
+        done
+    } < <(df -kP -x tmpfs -x devtmpfs -x overlay 2> /dev/null)
+
+    # df 这里的值是 1K-blocks 转换为 byte
+    disk_total_bytes=$((disk_total * 1024))
+    disk_used_bytes=$((disk_used * 1024))
+    disk_free_bytes=$((disk_free * 1024))
+
+    # 信息汇总
+    RESULT_DISK_PATH="$disk_root_path"
+    RESULT_DISK_INFO="$(format_bytes "$disk_used_bytes") / $(format_bytes "$disk_total_bytes")"
+    # shellcheck disable=SC2034
+    RESULT_DISK_FREE="$(format_bytes "$disk_free_bytes")"
+}
+
 # 执行基本系统信息检测
 get_system_info() {
     get_cpu_info
     get_mem_info
+    get_disk_info
 
     # 系统在线时间
     if is_have_cmd uptime; then
@@ -296,9 +343,10 @@ print_system_info() {
     else
         echo -e "VM-x/AMD-V\t: \xe2\x9c\x97 Disabled"
     fi
-
     echo -e "Memory\t\t: $RESULT_MEM_INFO"
     echo -e "Swap\t\t: $RESULT_SWAP_INFO"
+    echo -e "Space Disk\t: $RESULT_DISK_INFO"
+    echo -e "Boot Disk\t: $RESULT_DISK_PATH"
 
     echo -e "System Uptime\t: $SYSTEM_UPTIME"
     echo -e "Load Average\t: $LOAD_AVG"

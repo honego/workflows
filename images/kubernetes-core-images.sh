92af16eaa999172b5988293985cb07e889614a07
havario <157877551+havario@users.noreply.github.com>
2026-07-07 11:29:50 +0800

    -
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- images/kubernetes-core-images.sh
+++ images/kubernetes-core-images.sh
@@ -9,6 +9,7 @@ set -eEuo pipefail
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
+CORE_IMAGES_FILE="./kubernetes-core-images.md"
 : "${ALIYUN_REGISTRY:?missing ALIYUN_REGISTRY}"
 : "${ALIYUN_NAMESPACE:="$GITHUB_REPOSITORY_OWNER"}"
 
@@ -34,14 +35,14 @@ update_pause() {
     local latest_version current_version
 
     latest_version="$(skopeo list-tags docker://registry.k8s.io/pause | jq -r '.Tags[]' | grep -E '^[0-9]+(\.[0-9]+){1,2}$' | sort -V | tail -n 1)"
-    current_version="$(sed -En 's#^(.*/)?pause:([0-9]+(\.[0-9]+){1,2}(-[0-9]+)?)$#\2#p' kubernetes-core-images.md)"
+    current_version="$(sed -En 's#^(.*/)?pause:([0-9]+(\.[0-9]+){1,2}(-[0-9]+)?)$#\2#p' "$CORE_IMAGES_FILE")"
 
     if [[ "$(printf '%s\n%s\n' "$latest_version" "$current_version" | sort -V | head -n 1)" == "$latest_version" ]]; then
         return
     fi
 
     _log "Pause update: $current_version -> $latest_version"
-    sed -Ei "s#^((.*/)?pause:)[0-9]+(\.[0-9]+){1,2}(-[0-9]+)?\$#\1${latest_version}#" kubernetes-core-images.md
+    sed -Ei "s#^((.*/)?pause:)[0-9]+(\.[0-9]+){1,2}(-[0-9]+)?\$#\1${latest_version}#" "$CORE_IMAGES_FILE"
     sync_img "pause" "$latest_version"
 }
 

377da56d5c8a1f164ba3d0238e6e77cfc816ba12
havario <157877551+havario@users.noreply.github.com>
2026-07-07 11:16:23 +0800

    -
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- images/kubernetes-core-images.sh
+++ images/kubernetes-core-images.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0
+#
+# Description:
+# Copyright (c) 2026 honeok <i@honeok.com>
 
-set -eEu
+set -eEuo pipefail
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
+
+: "${ALIYUN_REGISTRY:?missing ALIYUN_REGISTRY}"
+: "${ALIYUN_NAMESPACE:="$GITHUB_REPOSITORY_OWNER"}"
+
+_log() {
+    printf '[%s] %s\n' "$(date '+%F %T')" "$*"
+}
+
+sync_img() {
+    local img="$1" tag="$2"
+
+    docker "pull registry.k8s.io/$img":"$tag"
+    docker tag "registry.k8s.io/$img":"$tag" "$ALIYUN_REGISTRY/$ALIYUN_NAMESPACE/$img":"$tag"
+    docker push "$ALIYUN_REGISTRY/$ALIYUN_NAMESPACE/$img":"$tag"
+}
 
 # Bump pause image version
 update_pause() {
@@ -13,7 +34,9 @@ update_pause() {
         return
     fi
 
+    _log "Pause update: $current_version -> $latest_version"
     sed -Ei "s#^((.*/)?pause:)[0-9]+(\.[0-9]+){1,2}(-[0-9]+)?\$#\1${latest_version}#" kubernetes-core-images.md
+    sync_img "pause" "$latest_version"
 }
 
 update_pause

472bb5d7249a9f70a73e09c24acb1ddcb1bafb7a
havario <157877551+havario@users.noreply.github.com>
2026-07-07 19:25:40 +0800

    -
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- images/kubernetes-core-images.sh
+++ images/kubernetes-core-images.sh
@@ -44,6 +44,32 @@ get_latest_ver() {
     skopeo list-tags "docker://$img" | jq -r '.Tags[]?' | grep -E -- "$regex" | sort -V | tail -n 1
 }
 
+get_current_ver() {
+    local img regex
+
+    img="$1"
+    regex="${2:-}"
+
+    if [ -z "$regex" ]; then
+        case "$img" in
+        *pause)
+            regex='^[0-9]+(\.[0-9]+){1,2}$'
+            ;;
+        *kube-apiserver | *kube-controller-manager | *kube-scheduler | *kube-proxy | *coredns/coredns)
+            regex='^v[0-9]+(\.[0-9]+){2}$'
+            ;;
+        *etcd)
+            regex='^[0-9]+(\.[0-9]+){2}-[0-9]+$'
+            ;;
+        *)
+            return 1
+            ;;
+        esac
+    fi
+
+    awk -F: -v img="$img" '$1 == img { print $2; exit }' "$CORE_IMAGES_FILE" | grep -E -- "$regex"
+}
+
 sync_img() {
     local img="$1" tag="$2"
 
@@ -58,7 +84,7 @@ update_pause() {
     local latest_ver current_ver
 
     latest_ver="$(get_latest_ver "registry.k8s.io/pause")"
-    current_ver="$(sed -En 's#^(.*/)?pause:([0-9]+(\.[0-9]+){1,2}(-[0-9]+)?)$#\2#p' "$CORE_IMAGES_FILE")"
+    current_ver="$(get_current_ver "registry.k8s.io/pause")"
 
     if [[ "$(printf '%s\n%s\n' "$latest_ver" "$current_ver" | sort -V | head -n 1)" == "$latest_ver" ]]; then
         return
@@ -81,3 +107,11 @@ update_pause
 # get_latest_ver "registry.k8s.io/kube-proxy"
 # get_latest_ver "registry.k8s.io/etcd"
 # get_latest_ver "registry.k8s.io/coredns/coredns"
+
+# get_current_ver "registry.k8s.io/pause"
+# get_current_ver "registry.k8s.io/kube-apiserver"
+# get_current_ver "registry.k8s.io/kube-controller-manager"
+# get_current_ver "registry.k8s.io/kube-scheduler"
+# get_current_ver "registry.k8s.io/kube-proxy"
+# get_current_ver "registry.k8s.io/etcd"
+# get_current_ver "registry.k8s.io/coredns/coredns"
